fb534d587931ec0340d3dda80dfa28260ac4ec37
Mirjalol9080 <pubgmabile969@gmail.com>
2024-09-19 16:07:24 +0500

    m

--- app.js
+++ app.js
@@ -2,6 +2,33 @@ const HeaderSearch = document.querySelector('.header-search');
 const searchPanel = document.querySelector('.search-panel');
 const headersearchclose = document.querySelector('.header-search__close');
 const headermobilebutton = document.querySelector('.header-mobile__button');
+const MenuButton = document.querySelector('.menu-button');
+const Modal = document.querySelector('.modal');
+const ModalBg = document.querySelector('.modal-bg');
+const ModalCloseBtn = document.querySelector('.modal-close__btn')
+
+
+MenuButton.addEventListener('click' , function(){
+    if(!Modal.classList.contains('open')){
+        Modal.classList.add('open');
+        searchPanel.classList.remove('open')
+        document.body.style.overflow = 'hidden';
+    }
+})
+
+ModalBg.addEventListener('click' , function(){
+    if(Modal.classList.contains('open')){
+        Modal.classList.remove('open');
+        document.body.style.overflow = 'hidden';
+    }
+})
+
+ModalCloseBtn.addEventListener('click' , function(){
+    if(Modal.classList.contains('open')){
+        Modal.classList.remove('open');
+        document.body.style.overflow = '';
+    }
+})
 
 headermobilebutton.addEventListener('click' , function(){
     searchPanel.classList.add('open')
